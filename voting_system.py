cde4cf0d895978d5ec3b41258ca649a358b45054
Mai Li McGhee <mjmcghee@dons.usfca.edu>
2026-06-16 17:18:17 -0700

    implemented election logic for Ranked choice voting, added functions generate_ballots() and run_election() and displays the results of each round with the overall winner at the end

--- voting_system.py
+++ voting_system.py
@@ -2,6 +2,7 @@ from candidate import Candidate
 from voter import Voter
 
 import random
+import heapq
 
 
 class VotingSystem:
@@ -28,11 +29,67 @@ class VotingSystem:
             for i in range(count)
         ]
 
+    #function that calculates distance between voter and candidate political leaning and ranks them for each voter ballet
+    def generate_ballots(self,count):
+        all_ballots = []
+        for voter in self.voters:
+            ballot = []
+            for canidate in self.candidates:
+                #calculate distance
+                distance = abs(voter.leaning - canidate.leaning)
+                heapq.heappush(ballot, (distance,canidate))
+            all_ballots.append(ballot)
+        return all_ballots
+
+    def run_election(self,ballots,num_candidates):
+        losers = set()
+        round = 1
+
+        while True:
+            pols = {candidate: 0 for candidate in self.candidates if candidate not in losers }
+            rank = []
+
+            #go through all first place votes
+            for ballot in ballots:
+                #get first choice
+                for distance, candidate in ballot:
+                    if candidate not in losers:
+                        pols[candidate] += 1
+                        break # if top choice is eliminated send vote to next highest choice
+           
+            total_votes = len(ballots)
+            pols = {candidate: (votes/total_votes)*100 for candidate, votes in pols.items()} #candidate % score
+            rank = sorted(pols, key=lambda k: pols[k], reverse=True) #candidate name sorted by % score
+
+            print("Round: ",round)
+            for candidate in rank:
+                print(f"{candidate.name}: {pols[candidate]:.2f}%")
+            
+            print()
+            round +=1
+
+            #calculate % of highest voted candidate
+            top_candidate = max(pols, key = lambda k: pols[k])#candidate name
+            percentage = (pols[top_candidate]/total_votes )* 100 #candidate % score
+
+            if percentage >= 50.0:
+                return top_candidate.name, percentage
+            else:
+                loosing_candidate = min(pols, key = lambda k: pols[k])#candidate name
+                del pols[loosing_candidate]
+                #adding votes from loseing candidates to the voters
+                leftover_votes = 0
+                percentage = pols[top_candidate]/total_votes * 100 
+                losers.add(loosing_candidate)
+        
+
+
 if __name__ == "__main__":
     voting_system = VotingSystem()
 
     voting_system.generate_candidates(5)
     voting_system.generate_voters(100)
+    ballots = voting_system.generate_ballots(100)
 
     print("Candidates:")
     for candidate in voting_system.candidates:
@@ -41,3 +98,8 @@ if __name__ == "__main__":
     print("\nVoters:")
     for voter in voting_system.voters:
         print(voter)
+
+    print("\nElection Results:")
+    winner = voting_system.run_election(ballots, 5)
+    print("\nWinner:")
+    print(winner[0], "with", winner[1], "% of the votes")
